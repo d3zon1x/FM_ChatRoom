419e743b5a40c96904b25afef73870532a49d4c9
serhiyji <serhiy22823@gmail.com>
2023-04-26 18:43:10 +0300

    Add button "кнопка"

--- ClientApp/MainWindow.xaml.cs
+++ ClientApp/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace ClientApp
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("boo");
+        }
     }
 }
