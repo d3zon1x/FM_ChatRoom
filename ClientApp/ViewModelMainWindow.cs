d3872709aaa6f7d2ee9d4389a3e3b7640a16ec36
Yura Bortnik <bortnik2604@gmail.com>
2023-04-27 22:06:59 +0300

    Added MainWindow and ViewModel for MainWindow

---
+++ ClientApp/ViewModelMainWindow.cs
@@ -0,0 +1,18 @@
+﻿using PropertyChanged;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientApp
+{
+    [AddINotifyPropertyChangedInterface]
+    public class ViewModelMainWindow
+    {
+        public ViewModelMainWindow()
+        {
+            
+        }
+    }
+}
