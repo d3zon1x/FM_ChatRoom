fc4c4b3395bd24bcd2e6d075a4ed57b9acb9ca02
serhiyji <serhiy22823@gmail.com>
2023-04-28 19:58:37 +0300

    add SingletonClass in ExpansionForCAndS

---
+++ ExpansionForCAndS/SingletonClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpansionForCAndS
+{
+    public abstract class SingletonClass<T> where T : class, new()
+    {
+        private static T _instance;
+        public static T Instance => (T)_instance ?? (_instance = new T());
+    }
+}
