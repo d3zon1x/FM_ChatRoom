433de57075aca75b3e186249f9fa2c420059b99a
Sost_sg54@student.itstep.org <Sost_sg54@student.itstep.org>
2023-04-28 20:24:12 +0300

    Create extensionsions class

---
+++ ExpansionForCAndS/Extensions.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace ExpansionForCAndS
+{
+    public static class Extensions
+    {
+        public static string ToBase64(this object obj)
+        {
+            string json = JsonConvert.SerializeObject(obj);
+            byte[] bytes = Encoding.UTF8.GetBytes(json);
+            return Encoding.Unicode.GetString(bytes);
+        } 
+        public static T FromBase64<T>(this string base64Text) 
+        {
+            byte[] bytes = Encoding.Unicode.GetBytes(base64Text);
+            string json = Encoding.Unicode.GetString(bytes);
+            return JsonConvert.DeserializeObject<T>(json);
+        }
+    }
+}
